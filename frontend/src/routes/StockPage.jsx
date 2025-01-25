6273b570011e401862a3a35645550b3ad35c725b
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2025-01-25 18:45:30 -0500

    Update stocks view

--- frontend/src/routes/StockPage.jsx
+++ frontend/src/routes/StockPage.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import PreviewStocks from "../components/StockPreview";
-import PreviewCrypto from "../components/CryptoPreview";
+import { stocks } from "../data/stocks";
+import { cryptos } from "../data/cryptos";
 
-const StockPage = () => {
-  return (
-    <>
-      <div>
-        <Navbar />
-        <h1>Stock Page</h1>
+function StockPage() {
+    const [data, setData] = useState([]);
+    const [stockList, setStockList] = useState(stocks);
 
-        <div className="flex flex-row gap-x-6 w-screen p-10">
-          <div className="bg-red-50 w-full">
-            <h4>Stocks</h4>
-            <PreviewStocks/>
-          </div>
-          <div className="bg-red-50 w-full">
-            <h4>Crypto</h4>
+    useEffect(() => {
+        // query backend for stock lists
+        setData({
+            stocks: stocks,
+            cryptos: cryptos,
+        });
+    }, []);
 
-            <PreviewCrypto/>
+    const setStocks = (stocksName) => {
+        if (data && data[stocksName]) {
+            setStockList(data[stocksName]);
+            return;
+        }
+        console.error("Stocks not found");
+    };
+
+    return (
+        <>
+            <div>
+                <Navbar />
+                <h1>Stock Page</h1>
+                <div className="flex justify-start">
+                    <button
+                        onClick={() => setStocks("stocks")}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Stocks
+                    </button>
+                    <button
+                        onClick={() => setStocks("cryptos")}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Cryptos
+                    </button>
+                </div>
+                <div className="flex flex-row justify-center gap-x-6 w-screen h-[50%] px-12">
+                    <div className="w-[100%]">
+                        <PreviewStocks dataSet={stockList} />
+                    </div>
+                </div>
             </div>
-
-        </div>
-      </div>
-    </>
-  );
-};
+        </>
+    );
+}
 
 export default StockPage;
