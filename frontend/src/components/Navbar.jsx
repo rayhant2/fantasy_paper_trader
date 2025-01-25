cdb7c4131598a50724fd59579509993a253eeb6a
rayhant2 <seen.rayhan@gmail.com>
2025-01-25 18:44:10 -0500

    h

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ReactLogo from "../assets"
 
 const Navbar = () => {
   return (
     <div>
+      <div className="w-screen px-4">
+  <div className="flex flex-row justify-between items-center">
+    {/* Left-aligned "Home" link */}
+    <div>
+      <div>
+        <Link to="/destination">
+          <img
+            src="https://via.placeholder.com/150"
+            alt="Clickable Image"
+            className="w-32 h-32"
+          />
+        </Link>
+      </div>
       <p>
         <Link to="/">Home</Link>
       </p>
+    </div>
+
+    {/* Right-aligned links */}
+    <div className="flex flex-row gap-5 ml-auto mr-20">
       <p>
         <Link to="/register">Register Page</Link>
       </p>
@@ -19,6 +37,10 @@ const Navbar = () => {
       <p>
         <Link to="/portfolio">Portfolio page</Link>
       </p>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 };
