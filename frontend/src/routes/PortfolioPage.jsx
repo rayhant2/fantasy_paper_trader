0d56f3fb5b355d6ed025b9e2e8ccc99d6e5aff39
rayhant2 <seen.rayhan@gmail.com>
2025-01-25 23:09:51 -0500

    Portfolio UI edits

--- frontend/src/routes/PortfolioPage.jsx
+++ frontend/src/routes/PortfolioPage.jsx
@@ -10,7 +10,7 @@ const PortfolioPage = () => {
       <div className="flex flex-row w-screen h-screen">
   {/* Left Section */}
   <div className="flex flex-col gap-5 my-5 ml-5 w-1/2">
-    <div className="bg-[#fcd46a] hover:shadow-md rounded-2xl p-4 flex-[0.25] transition-shadow duration-300 ease-in-out text-left items-center">
+    <div className="text-[#000000] bg-[#fcd46a] hover:shadow-md rounded-2xl p-4 flex-[0.25] transition-shadow duration-300 ease-in-out text-left items-center">
       <p className="text-s ml-2">Balance</p>
       <div className="flex flex-row gap-2 ml-2 items-center">
         <div className="flex flex-row items-end gap-2">
@@ -24,8 +24,8 @@ const PortfolioPage = () => {
 
     {/* Best/Worst Performing Stock */}
     <div className="flex flex-row gap-6">
-      <div className="bg-[#fcd46a] hover:shadow-md rounded-2xl p-5 flex-1 transition-shadow duration-300 ease-in-out text-left">
-        <p className="text-s">Best Performing Stock</p>
+      <div className="bg-[#fcd46a] text-[#000000] hover:shadow-md rounded-2xl p-5 flex-1 transition-shadow duration-300 ease-in-out text-left">
+        <p className="text-s">Strongest Performer</p>
         <div className="flex flex-row items-center gap-2">
             <p className="font-bold text-3xl">NVDA</p>
             <p>(▲  12%)</p>
@@ -33,8 +33,8 @@ const PortfolioPage = () => {
         </div>
       </div>
 
-      <div className="bg-[#fcd46a] hover:shadow-md rounded-2xl p-5 flex-1 transition-shadow duration-300 ease-in-out text-left">
-        <p className="text-s">Worst Performing Stock</p>
+      <div className="bg-[#fcd46a] text-[#000000] hover:shadow-md rounded-2xl p-5 flex-1 transition-shadow duration-300 ease-in-out text-left">
+        <p className="text-s">Weakest Performer</p>
         <div className="flex flex-row items-center gap-2">
             <p className="font-bold text-3xl">TSLA</p>
             <p>(▼  20%)</p>
@@ -44,14 +44,14 @@ const PortfolioPage = () => {
     </div>
 
     {/* Recent Transactions */}
-    <div className="bg-[#fcd46a] hover:shadow-md rounded-2xl p-5 flex-[0.75] transition-shadow duration-300 ease-in-out text-left overflow-auto">
-      <p className="text-s font-medium mb-4">Recent transactions:</p>
+    <div className="shadow-[inset_0_0_3px_5px_#F0F0F0] rounded-2xl p-3 flex-[0.75] text-left">
+      <p className="text-s font-medium ml-2 mt-2 mb-4 text-[#000000]">Recent transactions:</p>
     </div>
   </div>
 
   {/* Right Section */}
   <div className="bg-[#fcd46a] hover:shadow-lg rounded-2xl m-5 w-1/2 flex items-center justify-center transition-shadow duration-300 ease-in-out">
-    <h1>Graph Goes Here</h1>
+    <h1 className="text-[#000000]">Graph Goes Here</h1>
   </div>
 </div>
 
