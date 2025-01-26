e47e1037cb5ad8ae9c163520053228afdfd5b688
rayhant2 <seen.rayhan@gmail.com>
2025-01-25 19:55:29 -0500

    UI Update

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -25,18 +25,18 @@ export default function Registration() {
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+              <div className="flex flex-col items-start">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-900">
                   Email address
                 </label>
-                <div className="mt-2">
+                <div className="mt-2 w-full">
                   <input
                     id="email"
                     name="email"
                     type="email"
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
                   />
                 </div>
               </div>
@@ -62,7 +62,8 @@ export default function Registration() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md text-blue-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+
                 >
                   Sign in
                 </button>
--- frontend/src/routes/LoginPage.jsx
+++ frontend/src/routes/LoginPage.jsx
@@ -5,7 +5,7 @@ import Login from "../components/Login";
 const LoginPage = () => {
   return (
     <>
-    <div>
+    <div className="w-screen">
     <Navbar />
     <Login/>
     </div>
