324ec81f075fe69e6606ace1e7c10324dd06a1ee
rayhant2 <seen.rayhan@gmail.com>
2025-01-26 01:47:06 -0500

    useStates for Registration page

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -1,16 +1,35 @@
 import { Link } from "react-router-dom";
+import React, {useState} from "react";
 
 export default function Registration() {
+
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPass, setUserPass] = useState("");
+
+    const handleNameChange = (event) => {
+        setUserName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setUserEmail(event.target.value);
+    };
+
+    const handlePassChange = (event) => {
+        setUserPass(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Display Name:", userName);
+        console.log("Email:", userEmail);
+        console.log("Password:", userPass);
+        
+      };
+
     return (
         <>
-            {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
+            
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
@@ -24,7 +43,28 @@ export default function Registration() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+                    <form onSubmit={handleSubmit} className="space-y-6">
+                        <div className="flex flex-col items-start">
+                            <label
+                                htmlFor="display-name"
+                                className="block text-sm font-medium text-gray-900"
+                            >
+                                Display Name
+                            </label>
+                            <div className="mt-2 w-full">
+                                <input
+                                    id="name-input"
+                                    name="name-input"
+                                    type="text"
+                                    value={userName}
+                                    onChange={handleNameChange}
+                                    required
+                                    autoComplete="off"
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
                         <div className="flex flex-col items-start">
                             <label
                                 htmlFor="email"
@@ -37,6 +77,8 @@ export default function Registration() {
                                     id="email"
                                     name="email"
                                     type="email"
+                                    value={userEmail}
+                                    onChange={handleEmailChange}
                                     required
                                     autoComplete="email"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
@@ -58,6 +100,8 @@ export default function Registration() {
                                     id="password"
                                     name="password"
                                     type="password"
+                                    value={userPass}
+                                    onChange={handlePassChange}
                                     required
                                     autoComplete="current-password"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
